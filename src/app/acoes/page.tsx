066ea4ec9b793e0c2eb732fc5ce200a2ebeff19f
Rafael Simionato <rafaasimi@hotmail.com>
2024-03-14 10:59:05 -0300

    Aula: Renderização (Pt3)

--- src/app/acoes/page.tsx
+++ src/app/acoes/page.tsx
@@ -1,14 +1,22 @@
-export default async function AcoesPage() {
-  const response = await fetch('https://api.origamid.online/acoes/lua', {
-    next: {
-      revalidate: 5,
-    },
-  });
+'use client';
 
-  const acao = (await response.json()) as {
-    simbolo: string;
-    atualizada: string;
-  };
+import React from 'react';
+
+type Acao = {
+  simbolo: string;
+  atualizada: string;
+}
+
+export default function AcoesPage() {
+  const [acao, setAcao] = React.useState<Acao | null>(null);
+  
+  React.useEffect(() => {
+	  fetch('https://api.origamid.online/acoes/lua')
+	  .then((response) => response.json())
+	  .then((json) => setAcao(json));
+  }, []);
+  
+  if(acao === null) return null;
 
   return (
     <main>
@@ -17,3 +25,23 @@ export default async function AcoesPage() {
     </main>
   );
 }
+
+// export default async function AcoesPage() {
+//   const response = await fetch('https://api.origamid.online/acoes/lua', {
+//     next: {
+//       revalidate: 5,
+//     },
+//   });
+
+//   const acao = (await response.json()) as {
+//     simbolo: string;
+//     atualizada: string;
+//   };
+
+//   return (
+//     <main>
+//       <h1>{acao.simbolo}</h1>
+//       <h2>{acao.atualizada}</h2>
+//     </main>
+//   );
+// }
