6c746bb0c0ffe28edb2d944f1ad19c5b1e9cead6
Rafael Simionato <rafaasimi@hotmail.com>
2024-03-14 13:40:56 -0300

    Aula: Route Handlers (Pt3)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,4 +1,5 @@
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
 
 export async function GET() {
   const response = await fetch('https://api.origamid.online/conta/login', {
@@ -23,4 +24,11 @@ export async function GET() {
   });
 
   return Response.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  const param = request.nextUrl.searchParams.get('busca');
+  const body = await request.json();
+
+  return Response.json({ method: 'POST', param: param, body: body });
 }
