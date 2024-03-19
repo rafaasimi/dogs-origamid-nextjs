afe46d6ce864fee986bbea8adcfba94a3bcefd6e
Rafael Simionato <rafaasimi@hotmail.com>
2024-03-19 19:16:15 -0300

    Aula: Server Actions (Pt1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
+'use client';
+
+import { setCookie } from '@/actions/set-cookie';
 import Acesso from '@/components/acesso';
 import ServerFetch from '@/components/server-fetch';
 import Width from '@/components/width';
+import React from 'react';
 
-export default async function Home() {
+export default function Home() {
+  const [valor, setValor] = React.useState('');
+
+  async function handleClick() {
+    console.log('teste');
+    const response = await setCookie('segredo', '123456');
+    setValor(response.value);
+    console.log(response);
+  }
+
   return (
     <main>
       <h1>Home</h1>
       {/* <Acesso /> */}
       {/* <Width /> */}
-      <ServerFetch />
+      {/* <ServerFetch /> */}
+
+      <h2>Valor: {valor}</h2>
+      <button onClick={handleClick}>Definir cookie</button>
     </main>
   );
 }
