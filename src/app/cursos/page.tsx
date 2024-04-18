ca722ab8ce12fee89d91a9f57dc1805981cb370c
Rafael Simionato <rafaasimi@hotmail.com>
2024-04-18 09:14:10 -0300

    Aula: CSS modules

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,9 +1,8 @@
-
+import styles from './cursos.module.css';
 
 export default async function CursosPage() {
-
   return (
-    <main>
+    <main className={styles.cursos}>
       <h1>Front End e UI Design</h1>
       <p>A Origamid oferece cursos de Front End e UI Design</p>
     </main>
