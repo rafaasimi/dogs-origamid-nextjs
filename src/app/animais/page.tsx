540d267400533989879ad984144fef9d29d26223
Rafael Simionato <rafaasimi@hotmail.com>
2024-04-22 10:59:11 -0300

    Aula: Image (Pt3)

--- src/app/animais/page.tsx
+++ src/app/animais/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import styles from './animais.module.css';
+
 type Animal = {
   id: number;
   nome: string;
@@ -13,10 +15,11 @@ export default async function AnimaisPage() {
 
   return (
     <main>
+      <Image src="/imagens/login.jpg" width={1200} height={1600} alt="Dogs" sizes='100vw' priority/>
       <h1>Animais</h1>
 
-      <ul>
-        {animais.map((animal) => (
+      <ul className={styles.animais}>
+        {animais.map((animal, i) => (
           <li key={animal.id}>
             <h2>{animal.nome}</h2>
             <p>{animal.descricao}</p>
@@ -26,7 +29,8 @@ export default async function AnimaisPage() {
               width={2400}
               height={1600}
               quality={75}
-              sizes='100vw'
+              sizes="(max-width: 600px) 100vw, 50vw"
+              priority={i < 2}
             />
           </li>
         ))}
