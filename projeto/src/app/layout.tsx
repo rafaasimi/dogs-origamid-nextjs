dfcfd1435a6594fe8278fcd6625356874aceab07
Rafael Simionato <rafaasimi@hotmail.com>
2024-04-23 14:30:09 -0300

    Projeto: Header

--- projeto/src/app/layout.tsx
+++ projeto/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 import { type_second } from '@/functions/fonts';
+import Header from '@/components/Header/header';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={type_second.variable}>{children}</body>
+      <body className={type_second.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
