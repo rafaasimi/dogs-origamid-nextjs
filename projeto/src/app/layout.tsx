78f1980ea548cb0b0569b0b3182be212c9a820fc
Rafael Simionato <rafaasimi@hotmail.com>
2024-04-23 14:01:32 -0300

    Projeto: Font

--- projeto/src/app/layout.tsx
+++ projeto/src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
+import { type_second } from '@/functions/fonts';
 
 export const metadata: Metadata = {
-  title: "Dogs Next",
-  description: "Rede social para cachorros.",
+  title: 'Dogs Next',
+  description: 'Rede social para cachorros.',
 };
 
 export default function RootLayout({
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className={type_second.variable}>{children}</body>
     </html>
   );
 }
---
+++ projeto/src/functions/fonts.ts
@@ -0,0 +1,8 @@
+import { Spectral } from 'next/font/google';
+
+export const type_second = Spectral({
+  weight: ['700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--type-second-spectral',
+});
