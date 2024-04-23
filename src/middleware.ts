457d994170f7d6dda808ce4926cc80cd5a7afbce
Rafael Simionato <rafaasimi@hotmail.com>
2024-04-23 09:44:36 -0300

    Aula: Middleware (Pt2)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,23 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
+  const response = NextResponse.next();
 
-  if (token) {
-    return NextResponse.next();
-  } else {
+  if (request.nextUrl.pathname.startsWith('/entrar')) {
     return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (!token && request.nextUrl.pathname.startsWith('/acoes')) {
+    response.cookies.set('entrouAcoes', 'true', {
+      httpOnly: true,
+      secure: true,
+    });
+    return NextResponse.redirect(new URL('/login', request.url));
+  } else {
+    return response;
   }
 }
 
 export const config = {
-  matcher: ['/acoes'],
+  matcher: ['/((?!_next|favicon.ico).*)'],
 };
